609f9687100e1216fd3e2b34842feccbf171a996
Strengthless <40143583+Strengthless@users.noreply.github.com>
2018-07-03 21:43:22 +0800

    Removed automatic update (Music recent feed)
    Temporarily removed from the website due to the down time of the jokinami radio.

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ function toggleBookmarks() {
 
 function updateData() {
 //    setTimeout(updateData, 5000); 
-//    ^ [Currently removed due to the music stream downtime]
+//    ^^ [Currently removed due to the music stream downtime]
     $.getJSON("https://comet.shoutca.st/recentfeed/mushtunes/json/?nocache="+ (Math.floor(Math.random() * 1000)), (json) => {
       obj = json;
       if(obj !== objcache) {
