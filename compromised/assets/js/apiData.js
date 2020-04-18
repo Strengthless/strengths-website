8fc7e38404c58df7c0e20838aab69369881add4e
unknown <dickykam3@gmail.com>
2020-04-19 07:37:39 +0800

    v6.4 Staff check

--- compromised/assets/js/apiData.js
+++ compromised/assets/js/apiData.js
@@ -22,12 +22,11 @@ function getData() {
     $.getJSON('https://api.hypixel.net/player?key=' + apiKey + '&name=' + userID, function(data) {
       userName = data.player.displayname;
       userUUID = data.player.uuid;
-      mcVersion = data.player.mcVersionRp;
+      if (data.player.mcVersionRp) {mcVersion = data.player.mcVersionRp;} else {mcVersion = "An unknown client";};
       userLanguage = toTitleCase(data.player.userLanguage);
-      lastLogin = moment(data.player.lastLogin).format("MMMM Do YYYY, hh:mm:ss Z");
-      lastLogout = moment(data.player.lastLogout);
-      if (data.player.lastLogout-data.player.lastLogin > 0) {playTime = moment("6969-06-09").add(data.player.lastLogout-data.player.lastLogin, "ms").format("hh:mm:ss");} else {playTime = "N/A";};
-      mostRecentGame = toTitleCase(data.player.mostRecentGameType);
+      if (moment(data.player.lastLogin).isValid()) {lastLogin = moment(data.player.lastLogin).format("MMMM Do YYYY, hh:mm:ss Z");} else {lastLogin = "N/A";};
+      if (data.player.lastLogout) {if (data.player.lastLogout-data.player.lastLogin > 0) {playTime = moment("6969-06-09").add(data.player.lastLogout-data.player.lastLogin, "ms").format("hh:mm:ss");} else {playTime = "N/A";}} else {playTime = "<blur>N/A</blur>";};
+      if (data.player.mostRecentGameType) {mostRecentGame = toTitleCase(data.player.mostRecentGameType);} else {mostRecentGame = "N/A";};
       $("#apiData").empty();
       $("#apiData").append(`<br/><b>UUID:</b> ${userUUID}<br/><b>Username:</b> ${userName}<br/><br/><b>Version:</b> ${mcVersion}<br/><b>Language:</b> ${userLanguage}<br/><b>Last Login:</b> ${lastLogin}<br/><b>Playtime:</b> ${playTime}<br/><b>Most recent game:</b> ${mostRecentGame}<br/><br/>`);
     });
@@ -35,12 +34,11 @@ function getData() {
     $.getJSON('https://api.slothpixel.me/api/players/' + userID, function(data) {
       userName = data.username;
       userUUID = data.uuid;
-      mcVersion = data.mc_version;
+      if (data.mc_version) {mcVersion = data.mc_version;} else {mcVersion = "An unknown client";};
       userLanguage = toTitleCase(data.language);
-      lastLogin = moment(data.last_login).format("MMMM Do YYYY, hh:mm:ss Z");
-      lastLogout = moment(data.last_logout);
-      if (data.last_logout-data.last_login > 0) {playTime = moment("6969-06-09").add(data.last_logout-data.last_login, "ms").format("hh:mm:ss");} else {playTime = "N/A";};
-      mostRecentGame = toTitleCase(data.last_game);
+      if (moment(data.last_login).isValid()) {lastLogin = moment(data.last_login).format("MMMM Do YYYY, hh:mm:ss Z");} else {lastLogin = "<blur>N/A</blur>";};
+      if (data.last_logout) {if (data.last_logout-data.last_login > 0) {playTime = moment("6969-06-09").add(data.last_logout-data.last_login, "ms").format("hh:mm:ss");} else {playTime = "N/A";}} else {playTime = "<blur>N/A</blur>";};
+      if (data.last_game) {mostRecentGame = toTitleCase(data.last_game);} else {mostRecentGame = "<blur>N/A</blur>";};
       $("#apiData").empty();
       $("#apiData").append(`<br/><b>UUID:</b> ${userUUID}<br/><b>Username:</b> ${userName}<br/><br/><b>Version:</b> ${mcVersion}<br/><b>Language:</b> ${userLanguage}<br/><b>Last Login:</b> ${lastLogin}<br/><b>Playtime:</b> ${playTime}<br/><b>Most recent game:</b> ${mostRecentGame}<br/><br/>`);
     });
