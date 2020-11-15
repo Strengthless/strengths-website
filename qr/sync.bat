4b0703abdbba757cee4004bb5ec7cd553ebcf1fa
strengthless <dickykam3@gmail.com>
2020-11-15 09:17:08 +0800

    v9.1 Project QR draft

---
+++ qr/sync.bat
@@ -0,0 +1,2 @@
+@echo off
+FOR /L %%y IN (1, 1, 10) DO xcopy "D:\My files\Web projects\strengthless.club\qr\0\index.html" "D:\My files\Web projects\strengthless.club\qr\%%y" /Y /C
--- qr1/sync.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-FOR /L %%y IN (2, 1, 10) DO xcopy "D:\My files\Web projects\strengthless.club\qr\1\index.html" "D:\My files\Web projects\strengthless.club\qr\%%y" /Y /C
