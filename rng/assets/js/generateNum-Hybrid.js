6322572d29f3c103b4351ddcf74d3c0ec0a5c5f3
Strengthless <dickykam3@gmail.com>
2021-05-03 12:03:08 +0800

    YMCA YLD

--- rng/assets/js/generateNum-Hybrid.js
+++ rng/assets/js/generateNum-Hybrid.js
@@ -34,7 +34,7 @@ function generateNum() {
       $data = 0,
       index, change,
       letters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
-      lettersToAvoid = [4, 6, 8, 9, 10, 11, 13, 14, 16, 17, 20],
+      lettersToAvoid = [4, 6, 8, 9, 10, 11, 13, 14, 16, 17, 20, 27],
       lettersToExclude = [11],
       probabilityTable = [], finalResults = [],
       colors = ["green", "lightgreen"];
